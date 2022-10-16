9b7e990feb4ed52b9d276ce27c77b7f7a72d1e3c
Hadi Raza <hadiraza.edu@gmail.com>
2022-10-16 19:03:39 +0500

    Add files via upload

---
+++ My/html css js files/app.js
@@ -0,0 +1,78 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+let slideIndex1 = 1;
+showSlidess(slideIndex1);
+// Next/previous controls
+function plusSlidess(n) {
+  showSlidess(slideIndex1 += n);
+}
+// Thumbnail image controls
+function currentSlides(n) {
+  showSlidess(slideIndex1 = n);
+}
+function showSlidess(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides1");
+  let dots = document.getElementsByClassName("dot1");
+  if (n > slides.length) {slideIndex1 = 1}
+  if (n < 1) {slideIndex1 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex1-1].style.display = "block";
+  dots[slideIndex1-1].className += " active";
+}
+let slideIndex2 = 1;
+showSlidesss(slideIndex2);
+// Next/previous controls
+function plusSlidesss(n) {
+  showSlidesss(slideIndex2 += n);
+}
+// Thumbnail image controls
+function currentSlidess(n) {
+  showSlidesss(slideIndex2 = n);
+}
+function showSlidesss(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides2");
+  let dots = document.getElementsByClassName("dot2");
+  if (n > slides.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex2-1].style.display = "block";
+  dots[slideIndex2-1].className += " active";
+}
+var datee = new Date();
+var years = datee.getFullYear();
+document.getElementById('yearr').innerHTML = years;
